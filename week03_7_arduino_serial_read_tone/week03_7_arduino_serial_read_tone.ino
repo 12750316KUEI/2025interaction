7231a1b3e2c7a554eb5328de00d5b8555d2c6ef4
12750316KUEI <145088368+12750316KUEI@users.noreply.github.com>
2025-09-22 16:29:31 +0800

    week03

---
+++ week03_7_arduino_serial_read_tone/week03_7_arduino_serial_read_tone.ino
@@ -0,0 +1,16 @@
+//week03_7_arduino_serial_read_tone
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void serialEvent(){
+  while(Serial.available()) {
+    Serial.read();
+    tone(8,800,200);
+  }
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
